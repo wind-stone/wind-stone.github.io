1d1e8e2838af2bcb065affe63c63af2d1732fd01
chenxiaolong <chenxiaolong@kuaishou.com>
2024-10-10 00:13:25 +0800

    deploy

---
+++ assets/stability.html-DmZseRxe.js
@@ -0,0 +1,16 @@
+import { r as resolveComponent, c as createElementBlock, b as createStaticVNode, a as createVNode, o as openBlock } from "./app-CJaEk_oZ.js";
+import { _ as _export_sfc } from "./plugin-vue_export-helper-1tPrXgE0.js";
+const _sfc_main = {};
+function _sfc_render(_ctx, _cache) {
+  const _component_global_config = resolveComponent("global-config");
+  return openBlock(), createElementBlock("div", null, [
+    _cache[0] || (_cache[0] = createStaticVNode('<h1 id="稳定性" tabindex="-1"><a class="header-anchor" href="#稳定性"><span>稳定性</span></a></h1><h2 id="cdn-容灾" tabindex="-1"><a class="header-anchor" href="#cdn-容灾"><span>CDN 容灾</span></a></h2><h3 id="目标" tabindex="-1"><a class="header-anchor" href="#目标"><span>目标</span></a></h3><ul><li>CDN 容灾 <ul><li>某个 CDN 域名异常（比如 DNS 劫持、解析异常）时，可快速切换到新的域名</li></ul></li><li>实现 CDN 负载均衡、全局最优调度 <ul><li>防止 QPS 过高导致将 CDN 厂商打挂引起雪崩</li></ul></li></ul>', 4)),
+    createVNode(_component_global_config)
+  ]);
+}
+const stability_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "stability.html.vue"]]);
+const data = JSON.parse('{"path":"/front-end-engineering/performance-stability/stability.html","title":"稳定性","lang":"en-US","frontmatter":{"description":"稳定性 CDN 容灾 目标 CDN 容灾 某个 CDN 域名异常（比如 DNS 劫持、解析异常）时，可快速切换到新的域名 实现 CDN 负载均衡、全局最优调度 防止 QPS 过高导致将 CDN 厂商打挂引起雪崩","head":[["meta",{"property":"og:url","content":"https://blog.windstone.cc/front-end-engineering/performance-stability/stability.html"}],["meta",{"property":"og:site_name","content":"风动之石的博客"}],["meta",{"property":"og:title","content":"稳定性"}],["meta",{"property":"og:description","content":"稳定性 CDN 容灾 目标 CDN 容灾 某个 CDN 域名异常（比如 DNS 劫持、解析异常）时，可快速切换到新的域名 实现 CDN 负载均衡、全局最优调度 防止 QPS 过高导致将 CDN 厂商打挂引起雪崩"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2022-04-27T13:24:39.000Z"}],["meta",{"property":"article:modified_time","content":"2022-04-27T13:24:39.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"稳定性\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2022-04-27T13:24:39.000Z\\",\\"author\\":[]}"]]},"headers":[{"level":2,"title":"CDN 容灾","slug":"cdn-容灾","link":"#cdn-容灾","children":[{"level":3,"title":"目标","slug":"目标","link":"#目标","children":[]}]}],"git":{"createdTime":1651065879000,"updatedTime":1651065879000,"contributors":[{"name":"wind-stone","email":"wind-stone@qq.com","commits":1}]},"readingTime":{"minutes":0.24,"words":71},"filePathRelative":"front-end-engineering/performance-stability/stability.md","localizedDate":"April 27, 2022","autoDesc":true}');
+export {
+  stability_html as comp,
+  data
+};
